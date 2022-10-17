fca1d809ff81050bdb9f7e94d1fc1abc2cc5f531
Nguyen Nhu Thuong <thuongnn6666@gmail.com>
2022-10-17 10:46:52 +0700

    update new version

--- release.config.js
+++ release.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    branches: [
-        'master',
-        {
-            name: 'beta',
-            prerelease: true
-        }
-    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
@@ -15,14 +8,6 @@ module.exports = {
                 changelogFile: 'CHANGELOG.md'
             }
         ],
-        '@semantic-release/npm',
-        '@semantic-release/github',
-        [
-            '@semantic-release/git',
-            {
-                assets: ['CHANGELOG.md', 'dist/**'],
-                message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            }
-        ]
+        '@semantic-release/npm'
     ]
 }
